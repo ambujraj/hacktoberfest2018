6638b74dcad5e7413c50b93234a6c6e0fea074b9
Ambuj <thecoderguy@outlook.com>
2018-08-25 13:12:29 +0530

    calbyreference.cpp

---
+++ call-by-reference/callbyreference.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void swap(int &a, int &b)
+{
+    int temp;
+    temp = a;
+    a = b; 
+    b = temp; 
+
+    return;
+}
+int main () {
+   int x = 100;
+   int y = 200;
+ 
+   cout << "Before swap, value of a :" << x << endl;
+   cout << "Before swap, value of b :" << y << endl;
+   swap(x, y);
+
+   cout << "After swap, value of a :" << x << endl;
+   cout << "After swap, value of b :" << y << endl;
+ 
+   return 0;
+}
