3c2f4dd7d57f45a9584e2eb7850ab45bee24e243
Adit Mehta <aditmehta9@gmail.com>
2018-10-01 16:56:44 +0530

    Optimised and short code of factorial in C++

--- factorial/factorial.cpp
+++ factorial/factorial.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+
+
+int factorial(int n) 
+{ 
+    return (n==1 || n==0) ? 1: n * factorial(n - 1);  
+} 
+
+
 int main()
 {
     int num, fact;
     char c;
     while(true){
-    fact = 1;
-    cout<<"Enter the number you want to find factorial of: ";
-    cin>>num;
-    if(num==0){
-        fact = 1;
-    }
-    else if(num==1){
-        fact = 1;
-    }
-    else{
-        for (int i=num;i>0;i--){
-            fact = fact*i;
-        }
-    }
-    cout<<"Factorial of "<<num<<" is: "<<fact<<endl;
-    cout<<"Do You Want to Enter another number?(y/n): ";
-    cin>>c;
-    if(c=='n' || c=='N'){
-        exit(0);
-    }
-    else{
-        continue;
-    }
-    }
+		fact = 1;
+		cout << "Enter the number you want to find factorial of: ";
+		cin >> num;
+		factorial(num);
+		
+		//optimised and short code of factorial
+		cout <<"Factorial of "<< num <<" is: "<< fact << endl;
+		cout <<"Do You Want to Enter another number?(y/n): ";
+		cin>>c;
+		if(c=='n' || c=='N')
+		{
+			exit(0);
+		}
+		else
+		{
+			continue;
+		}
+	}
+return 0;
 }
