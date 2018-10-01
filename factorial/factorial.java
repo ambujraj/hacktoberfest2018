d5511cbf4bf2aa683d70f5726e5bb7b491d00ab2
svuckovic <sanjko10@gmail.com>
2018-10-01 18:08:50 +0200

    java implementation for palindrome

--- factorial/factorial.java
+++ factorial/factorial.java
@@ -1,10 +1,12 @@
-    class FactorialExample{  
-     public static void main(String args[]){  
-      int i,fact=1;  
-      int number=5;//It is the number to calculate factorial    
-      for(i=1;i<=number;i++){    
-          fact=fact*i;    
-      }    
-      System.out.println("Factorial of "+number+" is: "+fact);    
-     }  
-    }  
+public class Factorial {
+	static int factorial(int n) {
+		if (n == 0) {
+			return 1;
+		}
+		else {
+			return(n*factorial(n-1));
+		}
+	}
+}
+
+
