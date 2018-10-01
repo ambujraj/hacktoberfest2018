45d583887403db0c0315e2838efeb0f393ab7af0
Saurabh Bansal <b17059@students.iitmandi.ac.in>
2018-10-01 16:41:59 +0530

    Added Factorial.c

---
+++ factorial/factorial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int n; // No. for which Factorial is required
+	scanf("%d", &n);
+	int result = 1;
+
+	for(int i = 1; i <= n; i++)
+		result *= i;
+
+	printf("%d\n", result);
+	return 0;
+}
