4b34dd856ca434f3c96b997a85c8ac2afdd61adf
theartist007 <anuprava.livetowin@gmail.com>
2018-10-01 17:16:52 +0530

    Algorithm to calculate factorial added

---
+++ factorial/Anup_fact.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+long int factorial(int n)
+{
+	if(n==0)	return 1;
+	else return n*factorial(n-1);
+}
+
+int main()
+{
+	int n;
+	cout<<"Enter Number whose factorial is to be calculated : ";
+	cin>>n;
+	cout<<"The factorial of "<<n<<" is "<<factorial(n)<<endl;
+	return 0;
+}
