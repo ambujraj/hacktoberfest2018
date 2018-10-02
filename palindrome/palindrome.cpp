b1d6e91643e7d810bc5392840733324a99cdd879
destinyson7 <42719984+destinyson7@users.noreply.github.com>
2018-10-02 17:17:27 +0530

    Update palindrome.cpp
    Added comments to the code.

--- palindrome/palindrome.cpp
+++ palindrome/palindrome.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-#include <string>
-#include <stack>
+// Importing the required libraries
+#include <iostream> // for input and output
+#include <string> // for using string related operations
+#include <stack> // for importing the stack data structure
 
-bool isPalindrome(std::string word)
+bool isPalindrome(std::string word) //Function using stack data structure to push in letters of the word and the reversing the word
 {
     std::stack<char> temp;
     std::string reversed = "";
@@ -15,17 +16,17 @@ bool isPalindrome(std::string word)
        temp.pop();
     }
 
-    if (reversed == word)
-        return true;
+    if (reversed == word) // Checks if the reversed word is same as the actual word 
+        return true;      // If it is palindrome, returns true
     
-    else return false;
+    else return false;  //Else returns false
 }
 
 int main()
 {
     std::cout << "Enter a word!: ";
-    std::string word;
-    std::cin >> word;
+    std::string word;                     
+    std::cin >> word;          //Inputs word as a string
 
     isPalindrome(word) ? std::cout << word << " is a palindrome!" : std::cout << word << " is not a palindrome!";
     std::cout << std::endl;
