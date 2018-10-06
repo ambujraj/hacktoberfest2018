6a0d31e6b3ad4a3ec5aea140d61aeb24d6b2b041
Tanmay Srivastava <tanmays321@gmail.com>
2018-10-06 10:51:38 +0530

    Updated Java and JS file of Phone No. Validator

--- telephone-validator/validator.js
+++ telephone-validator/validator.js
@@ -1,4 +1,6 @@
-export default function validatePhoneNumber(phone_number = null){
-    let regex = /^\d(?:-\d{3}){3}\d$/
-    return regex.test(phone_number); //exemple: 2-123-123-1231 returns true
+export default function validatePhoneNumber(phone_number){
+    if(phone_number != null || phone_number.toString().length()==10 ){
+        return true;
+    }
+    return false;
 }
