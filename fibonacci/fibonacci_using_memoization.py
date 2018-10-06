20dcd65e9f5582da5258ade69439d6abb0ba7606
Omkar Pathak <omkarpathak27@gmail.com>
2018-10-06 16:58:52 +0530

    Create fibonacci_using_memoization.py

---
+++ fibonacci/fibonacci_using_memoization.py
@@ -0,0 +1,26 @@
+"""
+Fibonacci implementation through cache.
+"""
+
+def get_sequence(n):
+    """
+    Return Fibonacci sequence from zero to specified number.
+    """
+    cache = {0: 0, 1: 1}
+
+    def fib(num):
+        """
+        Return Fibonacci value by specified number as integer.
+        """
+        if num in cache:
+            return cache[num]
+        cache[num] = fib(num - 1) + fib(num - 2)
+        return cache[num]
+
+    def sequence(num):
+        """
+        Return sequence of Fibonacci values as list.
+        """
+        return [fib(value) for value in range(num + 1)]
+
+    return sequence(n)
