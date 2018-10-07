cd684a2ae207a88351e85def325da4925841aa00
Iván Arjona <ivanyn9@gmail.com>
2018-10-07 15:13:11 +0200

    Telephone validator in vue

---
+++ telephone-validator/validator.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <h1>Telephone Input Vue</h1>
+    <h2 style="color: #999">made with &#x2764; by Steven.</h2>
+    <div style="width: 500px; margin: 20px auto;">
+      <vue-tel-input @onInput="onInput"
+                     :preferredCountries="['us', 'gb', 'ua']">
+      </vue-tel-input>
+    </div>
+    <div v-if="phone.number"
+         style="color: #e83e8c">
+      <span>Number:
+        <strong>{{ phone.number }}</strong>,&nbsp;
+      </span>
+      <span>Is valid:
+        <strong>{{ phone.isValid }}</strong>,&nbsp;
+      </span>
+      <span>Country:
+        <strong>{{ phone.country }}</strong>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      phone: {
+        number: '',
+        isValid: false,
+        country: undefined,
+      },
+    };
+  },
+  methods: {
+    onInput({ number, isValid, country }) {
+      this.phone.number = number;
+      this.phone.isValid = isValid;
+      this.phone.country = country && country.name;
+    },
+  },
+};
+</script>
