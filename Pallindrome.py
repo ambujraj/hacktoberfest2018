f3a5bfacdf1c9fa0cddae69b6734b6415acc6c3e
imgsing <43990200+imgsing@users.noreply.github.com>
2018-10-09 17:02:57 +0530

    Update Pallindrome.py

--- Pallindrome.py
+++ Pallindrome.py
@@ -1,8 +1,8 @@
 
-print("Enter the string to check")
-t = input("Please enter a string")
+print("Enter the string to check-")
+t = input("Please enter a string-")
 
 if(t == t[::-1]):
-  print("The string entered is palindrome")
+  print("The string entered is palindrome :)")
 else:
-  print("Not a palindrome")
+  print("Not a palindrome :(")
