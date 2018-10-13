77c18f156f811f7401e993a5f1a6c16ba273e66e
udhaya kumar.S <udhayakumar241998@gmail.com>
2018-10-13 23:04:44 +0530

    Update Fatorial_finding.java

--- Fatorial_finding.java
+++ Fatorial_finding.java
@@ -4,9 +4,11 @@ class Factorial_finding
 {
    public static void main(String args[])
    {
-      int n, c, fact = 1;
+      int n;
+    int  c;
+    int fact = 1;
      
-      System.out.println("Enter an integer to calculate it's factorial");
+      System.out.println(" Enter an integer to calculate it's factorial");
       Scanner in = new Scanner(System.in);
      
       n = in.nextInt();
