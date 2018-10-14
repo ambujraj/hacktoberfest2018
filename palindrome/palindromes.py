8ab19425736ae141947bb1e175047581d5751d1c
Manoj Yadav <man007yadav@gmail.com>
2018-10-14 13:29:22 +0530

    palindrome

---
+++ palindrome/palindromes.py
@@ -0,0 +1,22 @@
+# function which return reverse of a string 
+def reverse(s): 
+    return s[::-1] 
+  
+def isPalindrome(s): 
+    # Calling reverse function 
+    rev = reverse(s) 
+  
+    # Checking if both string are equal or not 
+    if (s == rev): 
+        return True
+    return False
+  
+  
+# Driver code 
+s = "malayalam"
+ans = isPalindrome(s) 
+  
+if ans == 1: 
+    print("Yes") 
+else: 
+    print("No")
