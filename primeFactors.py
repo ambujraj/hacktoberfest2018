d60f3835f9796ab056424b8a2c34eb1d88e5f59e
Morteza Ghorbani Kari <30804459+mortezagk@users.noreply.github.com>
2018-10-14 21:34:16 +0330

    Create primeFactors.py

---
+++ primeFactors.py
@@ -0,0 +1,13 @@
+"""This Module retuens the prime Factors of the Given Number"""
+
+def primes(n):
+    primfac = []
+    d = 2
+    while d * d <= n:
+        while (n % d) == 0:
+            primfac.append(d)  # supposing you want multiple factors repeated
+            n //= d
+        d += 1
+    if n > 1:
+        primfac.append(n)
+    return primfac
