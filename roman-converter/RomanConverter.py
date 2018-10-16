98fc384e3c203d6141c5a96b1416cd065c259bac
paultflanagan <paultflanagan@gmail.com>
2018-10-16 15:53:06 -0400

    Publish of RomanConverter.py version 1.0

--- roman-converter/RomanConverter.py
+++ roman-converter/RomanConverter.py
@@ -1,15 +1,17 @@
-#Title:     RomanArabicConverter
-#Author:    Paul F.
-#Date:      15-Oct-2018
-#Description:
+# @Title:   RomanArabicConverter
+# @Version: 1.0
+# @Author:  Paul F.
+# @Date:    16-Oct-2018
+# Summary:
 #   Converts Roman Numerals to Arabic Numerals and vice versa
-#   Works with passing an argument or via log prompts
+#   Works with passing an argument or in manual mode via log prompts
 #   Goes up to 899 (DCCCXCIX)
-#Remaining Issues:
-#   CCD returns 500 despite having invalid formatting
-#       Likewise, XXC=>100, IIV=>5, IIIV=>6 etc.
-#To-Do:
-#   Sort out section for Roman>Arabic into "Before format is verified" and "After format is verified".
+#
+# Changelog:
+#   Date            Author          Description
+#   15-Oct-2018     Paul F.         First version, still some minor bugs with edge cases
+#   16-Oct-2018     Paul F.         Published version, fixed known buggy edge cases, cleaned code for readability
+#
 
 
 import sys
@@ -26,14 +28,25 @@ RomanValueDict = {
 #u = unus, q = quinque, d = decem 
 RomanOnesTriplet = {'u': 'I', 'q': 'V', 'd': 'X'}
 RomanTensTriplet = {'u': 'X', 'q': 'L', 'd': 'C'}
-RomanHndsTriplet = {'u': 'C', 'q': 'D', 'd': 'M'}   #Should never use the 'M', though
+RomanHndsTriplet = {'u': 'C', 'q': 'D', 'd': 'M'}   #Should never have to use the 'M', though
 
-#Not yet implemented
 RomanTripleTuple = (RomanOnesTriplet, RomanTensTriplet, RomanHndsTriplet)
+
+manualMode = False
+
+#Manual Mode Print function
+#Basically, only print if in Manual Mode
+def mmPrint(*objects):
+    if manualMode:
+        print(*objects)
+
 
 maxValueArabic = 899
 
 FinalResult = ""
+
+
+#Main loop start
 
 if (len(sys.argv) == 2):
     #input is argument 2
@@ -42,14 +55,16 @@ if (len(sys.argv) == 2):
 
 if (len(sys.argv) == 1):
     #get input via console
-    print("Error: No argument detected. Please enter input numeral (Roman or Arabic).")
+    manualMode = True
+    mmPrint("No argument detected. Entering manual mode. Please enter input numeral (Roman or Arabic).")
     inputValue = input("Value: ")
+    
 
 if (len(sys.argv) > 2):
     print("Error: Invalid number of arguments. Please input one Roman or Arabic numeral.")
     sys.exit()
 
-print("You have input: ", inputValue)
+mmPrint("You have input: ", inputValue)
 
 containsRoman = False
 containsArabic = False
@@ -71,57 +86,60 @@ for character in parsedList:
         containsArabic = True
     else:
         isInvalid = True
+        
 
 if ((containsRoman and containsArabic) or isInvalid):
     print("Error: Input is not a Roman or Arabic numeral.")
     sys.exit()
+    
 
 if (containsRoman):
-    print("Input is processed as a Roman numeral")
+    mmPrint("Input is processed as a Roman numeral")
     RValue = inputValue.upper()
     Sum = 0
     for index in range(len(RValue)):
         letter = RValue[index]
         digitSign = 1
         iFollowingNumerals = (len(RValue) - index) - 1
-        #print("iFollowingNumerals = ",iFollowingNumerals)
-        if iFollowingNumerals >= 1:   #At least one numeral following
-            if ((RomanValueDict[RValue[index+1]] == (RomanValueDict[letter] * 5)) or (RomanValueDict[RValue[index+1]] == RomanValueDict[letter] * 10)):
-                digitSign = -1         #Event of IV, IX, XL, XC, etc.
-                if iFollowingNumerals >= 2:
-                    if RomanValueDict[RValue[index+1]] <= RomanValueDict[RValue[index+2]]:
-                        print("Error: Improperly formatted Roman Numeral: improper numeral ordering type A.")
-                        sys.exit()
-            elif RomanValueDict[letter] < RomanValueDict[RValue[index+1]]:
-                print("Error: Improperly formatted Roman Numeral: improper numeral ordering type B.")
-                sys.exit()
-            elif iFollowingNumerals >= 3: #At least three numerals following
-                if RomanValueDict[letter] == RomanValueDict[RValue[index+1]] == RomanValueDict[RValue[index+2]] == RomanValueDict[RValue[index+3]]:
-                    print("Error: Improperly formatted Roman Numeral: over 3 successive matching numerals.")
+        if iFollowingNumerals >= 1:         #At least one numeral following
+            if RomanValueDict[letter] < RomanValueDict[RValue[index+1]]:    #IV, IX, IC, VX, etc.
+                if RomanValueDict[letter] in (1, 10, 100):
+                    if RomanValueDict[RValue[index+1]] in ((RomanValueDict[letter] * 5), (RomanValueDict[letter] * 10)):
+                        if (iFollowingNumerals >=2 and RomanValueDict[letter] == RomanValueDict[RValue[index+2]]): #IVI, XCX, etc.
+                            print("Error: Improperly formatted Roman Numeral: misplaced letters.")
+                            sys.exit()
+                        digitSign = -1          #Event of IV, IX, XL, XC, etc.
+                else:
+                    print("Error: Improperly formatted Roman Numeral: trailing greater number.")
                     sys.exit()
+            if iFollowingNumerals >= 2:         #At least two numerals following
+                if RomanValueDict[letter] == RomanValueDict[RValue[index+1]] and RomanValueDict[RValue[index+2]] in ((RomanValueDict[letter] * 5), (RomanValueDict[letter] * 10)):  #IIV, XXC, etc.
+                    print("Error: Improperly formatted Roman Numeral: this program uses Subtractive Notation.")
+                    sys.exit()
+                if iFollowingNumerals >= 3:         #At least three numerals following
+                    if RomanValueDict[letter] == RomanValueDict[RValue[index+1]] == RomanValueDict[RValue[index+2]] == RomanValueDict[RValue[index+3]]: #IIII, XXXX, etc.
+                        print("Error: Improperly formatted Roman Numeral: over 3 successive matching numerals.")
+                        sys.exit()
+        #Input character is verified correct
         Sum += (digitSign * RomanValueDict[letter])
     FinalResult = Sum
     
+    
 if (containsArabic):
-    print("Input is processed as an Arabic numeral")
+    mmPrint("Input is processed as an Arabic numeral")
     if int(inputValue) > maxValueArabic:
         print("Error: Input exceeds maximum value of ", maxValueArabic)
         sys.exit()
     for digit in range(len(parsedList)):
         DigitResult = ""
-        column = len(parsedList) - digit
-        if column == 3:     #In hundreds column
-            currentTriplet = RomanHndsTriplet
-        elif column == 2:   #In tens column
-            currentTriplet = RomanTensTriplet
-        else:               #In ones column
-            currentTriplet = RomanOnesTriplet
+        column = (len(parsedList) - digit) - 1
+        currentTriplet = RomanTripleTuple[column]
         digitValue = int(parsedList[digit])
         if digitValue%5 != 4:
             for i in range(digitValue%5):
                 DigitResult += currentTriplet['u']                      #I, II, III, X, XX, (...), CCC
             if digitValue >= 5:
-                DigitResult = currentTriplet['q'] + DigitResult         #V, VI, VII...
+                DigitResult = currentTriplet['q'] + DigitResult         #V, VI, VII, (...), DCCC
         elif digitValue == 4:
             DigitResult += currentTriplet['u'] + currentTriplet['q']    #IV, XL, CD
         elif digitValue == 9:
