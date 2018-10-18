cd7a11c54ab55be8d7c6fa576d8bccd7fd690521
Ansh Kapoor <32305554+AnshKapoor@users.noreply.github.com>
2018-10-18 23:45:28 +0530

    Generic Tree operations
    Added function to display size, to take input from user and display the data

--- Generic-Tree.java
+++ Generic-Tree.java
@@ -21,3 +21,39 @@ public class GenericTree {
 		Scanner s = new Scanner(System.in);
 		this.root = takeInput(s, null, 0);
 	}
+private Node takeInput(Scanner s, Node parent, int ithchild) {
+		if (parent == null) {
+			System.out.println("Enter the data for root Node");
+		} else {
+			System.out.println("Enter the data for " + ithchild + " th child of " + parent.data);
+		}
+		int nodeData = s.nextInt();
+		Node node = new Node(nodeData);
+		this.size++;
+		System.out.println("Please enter the number of children for " + node.data);
+		int numChildren = s.nextInt();
+		for (int i = 0; i < numChildren; i++) {
+			Node child = this.takeInput(s, node, i);
+			node.children.add(child);
+		}
+		return node;
+	}
+
+	public int size() {
+		return this.size;
+	}
+
+	public void display() {
+		this.display(this.root);
+	}
+
+	private void display(Node node) {
+		System.out.print(node.data + "=>");
+		for (int i = 0; i < node.children.size(); i++) {
+			System.out.print(node.children.get(i).data + ",");
+		}
+		System.out.println("END");
+		for (int i = 0; i < node.children.size(); i++) {
+			this.display(node.children.get(i));
+		}
+	}
