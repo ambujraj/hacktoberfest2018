af214c00fa48017980e05e307a05f9d01a60c687
jesiikmoreno <jessicamoreno@tecnomotum.com>
2018-10-23 13:09:34 -0500

    #99: A new proposal for validator.py

--- telephone-validator/validator.py
+++ telephone-validator/validator.py
@@ -2,8 +2,11 @@ import re
 
 def tel_num_validator(telephone_number):
     # Checks to see if input is a valid telephone number
-    return re.search("^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$",telephone_number)
+	return re.search("^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$",telephone_number)
 
 if __name__ == "__main__":
-    telephone_number = input("Telephone Number: ")
-    print(tel_num_validator(telephone_number))    
+	telephone_number = input("Telephone Number: ")
+	try:
+		phone=tel_num_validator(telephone_number)[0]
+	except Exception :
+		print ("Incorrect")
