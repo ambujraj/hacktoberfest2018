64a354a8d817d63696536c2a2cf0976abb712266
gsagariw <gsagariw@gmail.com>
2018-10-28 14:50:06 +0545

    Add docstring and type hinting

--- factorial/pyfactorial.py
+++ factorial/pyfactorial.py
@@ -1,8 +1,14 @@
 class Factorial:
 
+    """
+    Usage:
+    >>> print(Factorial.get_factorial(5))
+    [Output]: 120
+    """
+
     @staticmethod
-    def get_factorial(num: int):
-        if num == 0:
+    def get_factorial(number: int) -> int:
+        if number == 0:
             return 1
         else:
-            return num * Factorial.get_factorial(num - 1)
+            return number * Factorial.get_factorial(number - 1)
