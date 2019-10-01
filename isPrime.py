b2e9a5a4298f368de70dd0de91a2a07bf10c6dfd
qdtroemner <31808836+qdtroemner@users.noreply.github.com>
2019-10-01 08:04:34 -0400

    Modified formatting, added comments
    Made the prime numbers between lower and upper a little more human-readable, added more comments.

--- isPrime.py
+++ isPrime.py
@@ -9,13 +9,16 @@ upper = 1000
 #lower = int(input("Enter lower range: "))
 #upper = int(input("Enter upper range: "))
 
-print("Prime numbers between",lower,"and",upper,"are:")
+print(f"Prime numbers between {lower} and {upper} are:")
 
 for num in range(lower,upper + 1):
    # prime numbers are greater than 1
    if num > 1:
-       for i in range(2,num):
+       # divide by every number between 2 and half of the subject number (num)
+       for i in range(2,num // 2): # you only need to check to half of the subject number (num)
+           # is it evenly divisible by a number?
            if (num % i) == 0:
                break
        else:
+           # print if it is prime
            print(num)
