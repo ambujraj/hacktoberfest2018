d4f73c3d03f1ec41a9727a2b9dd965231505bbeb
niv789 <46987197+niv789@users.noreply.github.com>
2019-10-02 15:06:31 +0530

    update hello world
    included comments and took a user input

--- hello-world/HelloWorld.java
+++ hello-world/HelloWorld.java
@@ -1,8 +1,15 @@
+/* This is a simple Java program.*/
 public class HelloWorld {
-
+ // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+         System.out.println("Hello World!");
+         String s;
+         Scanner obj = new Scanner(System.in); /* create a object */ 
+         System.out.println("Enter your name:");
+         s = obj.nextLine(); /* Take string input and assign to variable */      
+         System.out.println("You entered string "+s); /* Print */ 
         
     }
     
