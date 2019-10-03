58f792f3e55e73cf232f8681b89ba0d6add28ac0
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-02 21:09:26 -0400

    Create palindrome.sh

---
+++ palindrome/palindrome.sh
@@ -0,0 +1,18 @@
+clear
+echo "Enter the number"
+read n
+number=$n
+reverse=0
+while [ $n -gt 0 ]
+do
+a=`expr $n % 10 `
+n=`expr $n / 10 `
+reverse=`expr $reverse \* 10 + $a`
+done
+echo $reverse
+if [ $number -eq $reverse ]
+then
+    echo "Number is palindrome"
+else
+	echo "Number is not palindrome"
+fi
