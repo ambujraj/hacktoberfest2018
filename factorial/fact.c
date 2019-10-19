9522cbe10fe46322a50b72af7c5f1064ae213b62
AnkitaAditya20 <ankita.aditya20@gmail.com>
2019-10-20 03:46:30 +0530

    Update Factorial code for all the possible cases

--- factorial/fact.c
+++ factorial/fact.c
@@ -1,15 +1,18 @@
-#include <stdio.h>
-
-
-void main()
+int main()
 {
-	int a;
-	scanf("%d", &a);
-	int res = 1;
-	
-	while(a)
-	{res*=a;
-	a--;}
-
-	printf("%d\a", res);
+    int n, i;
+    unsigned long long factorial = 1;
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            factorial *= i;            
+        }
+        printf("Factorial of %d = %llu", n, factorial);
+    }
+    return 0;
 }
