0826a1e090e369a2b5210c043667e7bbadcda235
UlVc <ulvc@ciencias.unam.mx>
2019-10-24 14:08:44 -0500

    Improved code of factorial.py

--- factorial.py
+++ factorial.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python2
 
 num = int(raw_input("enter no. for calculating its factorial: "))
-fact=1
-for i in range(1,num+1):
-	fact=fact*i
+fact = 1
+
+for i in range(1, num + 1):
+    fact *= i
+
 print "factorial is:",fact
