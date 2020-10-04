b0e99d9ef2681b5f498627a8beb2947c7b6f0145
Sahil kumar <900213327sahil@gmail.com>
2020-10-04 18:49:03 +0530

    updated functions.py
    changed whole program two to three functions are updated
    play_game() is the main function, which performs following tasks :
    
    Calls create_board() to create a 9×9 board and initializes with 0.
    For each player (1 or 2), calls the random_place() function to randomly choose a location on board and mark that location with the player number, alternatively.
    Print the board after each move.
    Evaluate the board after each move to check whether a row or column or a diagonal has the same player number. If so, displays the winner name. If after 9 moves, there are no winner then displays -1.

--- Tic Tac Toe/Functions.py
+++ Tic Tac Toe/Functions.py
@@ -1,115 +1,116 @@
-def col_check(a,b,c):                  
-    for i in range(3):
-        if a[i]==b[i]==c[i]:
-            print a[i]," is Winner"
-            return 1
-            break
+# Tic-Tac-Toe Program using 
+# random number in Python 
 
+# importing all necessary libraries 
+import numpy as np 
+import random 
+from time import sleep 
 
-def row_check(a,b,c):                           
-    for i in range(1):
-        if a[i]==a[i+1]==a[i+2]:                
-            print a[i]," is Winner"
-            return 1
-            break
-        elif b[i]==b[i+1]==b[i+2]:
-            print b[i]," is Winner"
-            return 1
-            break
-        elif c[i]==c[i+1]==c[i+2]:             
-            print c[i]," is Winner"
-            return 1
-            break
+# Creates an empty board 
+def create_board(): 
+	return(np.array([[0, 0, 0], 
+					[0, 0, 0], 
+					[0, 0, 0]])) 
 
+# Check for empty places on board 
+def possibilities(board): 
+	l = [] 
+	
+	for i in range(len(board)): 
+		for j in range(len(board)): 
+			
+			if board[i][j] == 0: 
+				l.append((i, j)) 
+	return(l) 
 
-def diag_check(a,b,c):
-    i=0
-    if a[i]==b[i+1]==c[i+2]:
-        print a[i]," is Winner"
-        return 1
-    elif c[i]==b[i+1]==a[i+2]:
-        print c[i]," is Winner"
-        return 1
+# Select a random place for the player 
+def random_place(board, player): 
+	selection = possibilities(board) 
+	current_loc = random.choice(selection) 
+	board[current_loc] = player 
+	return(board) 
 
+# Checks whether the player has three 
+# of their marks in a horizontal row 
+def row_win(board, player): 
+	for x in range(len(board)): 
+		win = True
+		
+		for y in range(len(board)): 
+			if board[x, y] != player: 
+				win = False
+				continue
+				
+		if win == True: 
+			return(win) 
+	return(win) 
 
-def col_check_ai(a,b,c):
-    for i in range(3):
-        if a[i]==b[i]=='O'and c[i]!='X'and c[i]!='O':
-            c[i]='X'
-            return 1
-        elif b[i]==c[i]=='O'and a[i]!='X'and a[i]!='O':
-            a[i]='X'
-            return 1
-        elif a[i]==c[i]=='O'and b[i]!='X'and b[i]!='O':
-            b[i]='X'
-            return 1
-    return 0
+# Checks whether the player has three 
+# of their marks in a vertical row 
+def col_win(board, player): 
+	for x in range(len(board)): 
+		win = True
+		
+		for y in range(len(board)): 
+			if board[y][x] != player: 
+				win = False
+				continue
+				
+		if win == True: 
+			return(win) 
+	return(win) 
 
+# Checks whether the player has three 
+# of their marks in a diagonal row 
+def diag_win(board, player): 
+	win = True
+	y = 0
+	for x in range(len(board)): 
+		if board[x, x] != player: 
+			win = False
+	if win: 
+		return win 
+	win = True
+	if win: 
+		for x in range(len(board)): 
+			y = len(board) - 1 - x 
+			if board[x, y] != player: 
+				win = False
+	return win 
 
-def row_check_ai(a,b,c):
-    k=[a,b,c]
-    for i in k:
-        if i[1]==i[2]=='O'and i[0]!='X'and i[0]!='O':
-            i[0]='X'
-            return 1
-        elif i[0]==i[2]=='O'and i[1]!='X'and i[1]!='O':
-            i[1]='X'
-            return 1
-        elif i[1]==i[0]=='O' and i[2]!='X'and i[2]!='O':
-            i[2]='X'
-            return 1
-    return 0
+# Evaluates whether there is 
+# a winner or a tie 
+def evaluate(board): 
+	winner = 0
+	
+	for player in [1, 2]: 
+		if (row_win(board, player) or
+			col_win(board,player) or
+			diag_win(board,player)): 
+				
+			winner = player 
+			
+	if np.all(board != 0) and winner == 0: 
+		winner = -1
+	return winner 
 
+# Main function to start the game 
+def play_game(): 
+	board, winner, counter = create_board(), 0, 1
+	print(board) 
+	sleep(2) 
+	
+	while winner == 0: 
+		for player in [1, 2]: 
+			board = random_place(board, player) 
+			print("Board after " + str(counter) + " move") 
+			print(board) 
+			sleep(2) 
+			counter += 1
+			winner = evaluate(board) 
+			if winner != 0: 
+				break
+	return(winner) 
 
-def corner_check_ai(a,b,c):
-    if a[0]!='X'and a[0]!='O':
-        a[0]='X'
-        return 1
-    elif c[0]!='X'and c[0]!='O':
-        c[0]='X'
-        return 1
-    elif a[2]!='X'and a[2]!='O':
-        a[2]='X'
-        return 1
-    elif c[2]!='X'and c[2]!='O':
-        c[2]='X'
-        return 1
-    else:
-        return 0
-
-
-def winning_move_ai(a,b,c):
-    k=[a,b,c]
-    for i in k:
-        if i[1]==i[2]=='X'and i[0]!='X'and i[0]!='O':
-            i[0]='X'
-            return 1
-        elif i[0]==i[2]=='X'and i[1]!='X'and i[1]!='O':
-            i[1]='X'
-            return 1
-        elif i[1]==i[0]=='X' and i[2]!='X'and i[2]!='O':
-            i[2]='X'
-            return 1
-    for i in range(3):
-        if a[i]==b[i]=='O'and c[i]!='X'and c[i]!='O':
-            c[i]='X'
-            return 1
-        elif b[i]==c[i]=='O'and a[i]!='X'and a[i]!='O':
-            a[i]='X'
-            return 1
-        elif a[i]==c[i]=='O'and b[i]!='X'and b[i]!='O':
-            b[i]='X'
-            return 1
-    if a[0]==b[1] and c[2]!='X'and c[2]!='O':
-        c[2]='X'
-        return 1
-    elif c[0]==b[1] and a[2]!='X'and a[2]!='O':
-        a[2]='X'
-        return 1
-    elif c[2]==b[1] and a[0]!='X'and a[0]!='O':
-        a[0]='X'
-        return 1
-    elif a[2]==b[1] and c[0]!='X'and c[0]!='O':
-        c[0]='X'
-        return 1
-    return 0
+# Driver Code 
+print("Winner is: " + str(play_game())) 
