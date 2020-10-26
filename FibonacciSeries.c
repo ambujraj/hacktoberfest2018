e7f7bbff6a5ad2d2a899be7af8e998ce81ef65db
dvilChandra <57448981+dvilChandra@users.noreply.github.com>
2020-10-26 11:08:08 +0530

    Update FibonacciSeries.c
    simple will less variables;

--- FibonacciSeries.c
+++ FibonacciSeries.c
@@ -1,19 +1,30 @@
-#include <stdio.h>
+#include<stdio.h>
+ 
+int Fibonacci(int);
+ 
 int main()
 {
-    int i, n, t1 = 0, t2 = 1, nextTerm;
-
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-
-    printf("Fibonacci Series: ");
-
-    for (i = 1; i <= n; ++i)
-    {
-        printf("%d, ", t1);
-        nextTerm = t1 + t2;
-        t1 = t2;
-        t2 = nextTerm;
-    }
-    return 0;
+   int n, i = 0, c;
+ 
+   scanf("%d",&n);
+ 
+   printf("Fibonacci series\n");
+ 
+   for ( c = 1 ; c <= n ; c++ )
+   {
+      printf("%d\n", Fibonacci(i));
+      i++; 
+   }
+ 
+   return 0;
+}
+ 
+int Fibonacci(int n)
+{
+   if ( n == 0 )
+      return 0;
+   else if ( n == 1 )
+      return 1;
+   else
+      return ( Fibonacci(n-1) + Fibonacci(n-2) );
 }
