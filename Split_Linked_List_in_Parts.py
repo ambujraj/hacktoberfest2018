76456d9a1e07e3fd803a6dafda203779e1ccb5da
satty08 <54195003+satty08@users.noreply.github.com>
2021-10-01 16:00:41 +0530

    Create Split_Linked_List_in_Parts.py

---
+++ Split_Linked_List_in_Parts.py
@@ -0,0 +1,32 @@
+'''
+Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts.
+
+The length of each part should be as equal as possible: no two parts should have a size differing by more than one. This may lead to some parts being null.
+
+The parts should be in the order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal to parts occurring later.
+
+Return an array of the k parts.
+'''
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def splitListToParts(self, root, k):
+        cur = root
+        for N in range(1001):
+            if not cur: break
+            cur = cur.next
+        width, remainder = divmod(N, k)
+
+        ans = []
+        cur = root
+        for i in range(k):
+            head = write = ListNode(None)
+            for j in range(width + (i < remainder)):
+                write.next = write = ListNode(cur.val)
+                if cur: cur = cur.next
+            ans.append(head.next)
+        return ans
