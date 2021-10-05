4aeaa3815e01e494fd9e64dcfbe23dd0edfc410b
Rohit Singh <RohitSinghSays21@gmail.com>
2021-10-05 20:49:47 +0530

    Update fibonacci.py

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,7 +1,6 @@
 def F(n):
-    if n == 0: return 0
-    elif n == 1: return 1
-    else: return F(n-1)+F(n-2)
+    if n in [0,1] return n
+    return F(n-1)+F(n-2)
 
 if __name__ == '__main__':
     num = int(input("Enter a number: "))
