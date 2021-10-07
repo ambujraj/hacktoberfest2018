2da6f9cc3ac7969c74a7736679a014d5264322d2
Laksh_coder <71703674+Laksh-Devloper@users.noreply.github.com>
2021-10-07 13:55:13 +0530

    Created Notepad GUI Using Tkinter Module in Python
    For hacktoberfest 
    
    Name: Laksh
    Place: India , M.P
    About: CS Student
    Programming Language:
    HTML
    Css
    Js
    C
    Python
    Email: hackersarenayt22@gmail.com
    
    
    #1356 
    #hacktoberfest-accepted
    @sn0opy @dduleone @arghasen @abc-xx @Ditti4

---
+++ notepad_gui.py
@@ -0,0 +1,26 @@
+# Developed by Laksh
+from tkinter import *
+from tkinter.messagebox import showinfo
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import os
+
+def newFile():
+    global file
+    root.title("Untitled - Notepad [By Laksh]")
+    file = None
+    TextArea.delete(1.0, END)
+
+
+def openFile():
+    global file
+    file = askopenfilename(defaultextension=".txt",
+                           filetypes=[("All Files", "*.*"),
+                                     ("Text Documents", "*.txt")])
+    if file == "":
+        file = None
+    else:
+        root.title(os.path.basename(file) + " - Notepad")
+        TextArea.delete(1.0, END)
+        f = open(file, "r")
+        TextArea.insert(1.0, f.read())
+        f.close()
